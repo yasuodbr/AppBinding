dc7323fa877531e34081b2421c6edff6d290a52d
Gustavoh345 <felipesardinham@gmail.com>
2026-08-18 22:31:22 -0300

    Adição de Views

--- AppBindingCommands/App.xaml.cs
+++ AppBindingCommands/App.xaml.cs
@@ -7,12 +7,36 @@ namespace AppBindingCommands
         public App()
         {
             InitializeComponent();
+
+            DateTime data = DateTime.Now;
+            Preferences.Set("dtAtual", data);
+            Preferences.Set("AcaoInicial", string.Format("* App executado ás {0}. \n", data));
+
+            MainPage = new AppShell();
         }
         //comentario 
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
-        }
+        
     }
+
+        protected override void OnStart()
+        {
+                base.OnStart();
+            Preferences.Set("AcaoStart", string.Format("* App iniciado ás {0}. \n", DateTime.Now));
+        }
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            Preferences.Set("AcaoSleep", string.Format("* App em segundo plano ás {0}. \n", DateTime.Now));
+
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Preferences.Set("AcaoResume", string.Format("* App reativado ás {0}. \n", DateTime.Now));
+
+        }
 }
--- AppBindingCommands/MainPage.xaml.cs
+++ AppBindingCommands/MainPage.xaml.cs
@@ -11,14 +11,31 @@
 
         private void OnCounterClicked(object? sender, EventArgs e)
         {
-            count++;
+            //count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            //if (count == 1)
+               // CounterBtn.Text = $"Clicked {count} time";
+          // else
+              //  CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+           // SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void btnAtualizarInformacoes_Clicked(object sender, EventArgs e)
+        {
+            string informacoes = string.Empty;
+
+            if (Preferences.ContainsKey("AcaoInicial"))
+                informacoes += Preferences.Get("AcaoInicial", string.Empty);
+
+            if (Preferences.ContainsKey("AcaoStart"))
+                informacoes += Preferences.Get("AcaoStart", string.Empty);
+
+            if (Preferences.ContainsKey("AcaoSleep")) informacoes += Preferences.Get("AcaoSleep", string.Empty);
+
+            if (Preferences.ContainsKey("AcaoResume")) informacoes += Preferences.Get("AcaoResume", string.Empty);
+
+            lblInformacoes.Text = informacoes;
         }
     }
 }
